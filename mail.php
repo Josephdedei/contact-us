9788c5b754600849811b02b552e4c8540e87d0b6
Josephdedei <dedeijoseph602@gmail.com>
2025-01-27 04:53:56 -0800

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,37 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject']
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "josephabeba@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) 0r die("Error");
+echo'
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact form</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1>THANK YOU FOR CONTACTING US. GOD LOVES YOU.</h1>
+      <p class="back">Go back to <a href="index.html">homepage</a>.</p>
+      
+    </div>
+  </body>
+</html>
+
+';
+
+?>
